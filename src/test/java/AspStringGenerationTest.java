5ac5399639a250ec19b85601de8f4cb754212871
Juri Lozowoj <juri.lozowoj@freenet.de>
2019-11-18 12:49:07 +0100

    added requested changes

--- src/test/java/AspStringGenerationTest.java
+++ src/test/java/AspStringGenerationTest.java
@@ -1,32 +1,33 @@
-import de.unikassel.vs.asp_modelling.ASPGenerator;
+
+import de.unikassel.vs.asp.modelling.AspGenerator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class APSStringGenerationTest {
+public class AspStringGenerationTest {
 
     @Test
-    public void predicateStringTest(){
+    public void predicateStringTest() {
         String testString = "fly(X) :- bird(X), not -fly(X).";
 
-        ASPGenerator gen = new ASPGenerator();
+        AspGenerator gen = new AspGenerator();
 
         gen.createRule().addHead().withPredicate("fly").withVariableAsRule("X")
                 .addBody().withPredicate("bird").withVariableAsBody("X").withPredicate("not -fly").withVariableAsBody("X");
 
-        Assertions.assertEquals(testString, gen.getASPProgram());
+        Assertions.assertEquals(testString, gen.getAspProgram());
     }
 
     @Test
-    public void factStringTest(){
+    public void factStringTest() {
 
         String testString = "eagle(eddy).\npenguin(tux).";
 
-        ASPGenerator gen = new ASPGenerator();
+        AspGenerator gen = new AspGenerator();
 
         gen.createFact("eagle").withConstant("eddy");
         gen.createFact("penguin").withConstant("tux");
 
-        Assertions.assertEquals(testString, gen.getASPProgram());
+        Assertions.assertEquals(testString, gen.getAspProgram());
     }
 
 }
