304d97d03a7a2a4d97a2cbd79d0650db0a14a46c
Dominik Waas <dominikwaas@gmx.de>
2019-11-19 16:22:58 +0100

    Class for Constants

---
+++ src/main/java/de/unikassel/vs/asp/modelling/syntax/Constant.java
@@ -0,0 +1,12 @@
+package de.unikassel.vs.asp.modelling.syntax;
+
+public class Constant {
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
