9f19714f1f96baeec7b33537933cf84b1ddbad98
Dominik Waas <dominikwaas@gmx.de>
2019-11-19 16:28:21 +0100

    Ability to set constants which the Variable cant be

--- src/main/java/de/unikassel/vs/asp/modelling/syntax/Variable.java
+++ src/main/java/de/unikassel/vs/asp/modelling/syntax/Variable.java
@@ -1,7 +1,20 @@
 package de.unikassel.vs.asp.modelling.syntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Variable {
     private String name;
+    private List<Constant> constantsOutOfValueRange = new ArrayList<>();
+
+    public List<Constant> getConstantsOutOfValueRange() {
+        return constantsOutOfValueRange;
+    }
+
+    public void setConstantsOutOfValueRange(List<Constant> constantsOutOfValueRange) {
+        this.constantsOutOfValueRange = constantsOutOfValueRange;
+    }
+
     public String getName() {
         return name;
     }
