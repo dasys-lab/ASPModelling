53037d2e80915c9c933a091316054d21e7ba64a3
Marc-Tell <tell96@freenet.de>
2019-11-21 14:34:24 +0100

    Add option to set bounds on regula choice

--- src/main/java/de/unikassel/vs/asp/modelling/syntax/Choice.java
+++ src/main/java/de/unikassel/vs/asp/modelling/syntax/Choice.java
@@ -2,12 +2,89 @@ package de.unikassel.vs.asp.modelling.syntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
 
 public class Choice extends PredicateTerm {
+
+    private Bound lowerBound;
+    private Bound upperBound;
+
     private List<Predicate> predicates;
 
     public Choice() {
         predicates = new ArrayList<Predicate>();
+    }
+
+    /**
+     * Standard getter.
+     *
+     * @return lower bound as optional (empty if no bound)
+     */
+    public OptionalInt getLowerBound() {
+        return lowerBound == null ? OptionalInt.empty() : OptionalInt.of(lowerBound.value);
+    }
+
+    /**
+     * Set a lower bound for this Choice.
+     *
+     * @param bound the bound to set
+     * @return this
+     */
+    public Choice withLowerBound(int bound) {
+        this.lowerBound = new Bound(bound);
+        return this;
+    }
+
+    /**
+     * Remove the lower bound for this Choice.
+     *
+     * @return this
+     */
+    public Choice withoutLowerBound() {
+        this.lowerBound = null;
+        return this;
+    }
+
+    /**
+     * Standard getter.
+     *
+     * @return upper bound as optional (empty if no bound)
+     */
+    public OptionalInt getUpperBound() {
+        return upperBound == null ? OptionalInt.empty() : OptionalInt.of(upperBound.value);
+    }
+
+    /**
+     * Set a upper bound for this Choice.
+     *
+     * @param bound the bound to set
+     * @return this
+     */
+    public Choice withUpperBound(int bound) {
+        this.upperBound = new Bound(bound);
+        return this;
+    }
+
+    /**
+     * Remove the upper bound for this Choice.
+     *
+     * @return this
+     */
+    public Choice withoutUpperBound() {
+        this.upperBound = null;
+        return this;
+    }
+
+    /**
+     * Choice with an exact value.
+     *
+     * Sets both bounds.
+     *
+     * @param exact the exact value to be matched
+     * @return this
+     */
+    public Choice withExact(int exact) {
+        return this.withLowerBound(exact).withUpperBound(exact);
     }
 
     public List<Predicate> getPredicates() {
@@ -23,5 +100,14 @@ public class Choice extends PredicateTerm {
         return "{"
                 + String.join("; ", predicates.stream().map(Predicate::getName).toArray(String[]::new))
                 + "}";
+    }
+
+    private static class Bound {
+
+        private final int value;
+
+        private Bound(int value) {
+            this.value = value;
+        }
     }
 }
